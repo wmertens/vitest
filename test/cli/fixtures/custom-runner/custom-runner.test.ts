55a96d573f3d1c3314e8507063f36a6ea59dca0b
Wout Mertens <Wout.Mertens@gmail.com>
2025-01-28 23:04:19 +0100

    fix(runner): correctly initialize suite in task
    this adds a test with a custom runner that allows you to wait for other tests, even when they weren't scheduled. It requires `suite` to be defined at context extension time.

---
+++ test/cli/fixtures/custom-runner/custom-runner.test.ts
@@ -0,0 +1,57 @@
+import {describe, expect, test} from 'vitest'
+
+declare module 'vitest' {
+	interface TestContext {
+		result: (name: string) => Promise<unknown>
+	}
+}
+
+describe('await multiple prior test result', () => {
+	test('first', async () => {
+		expect(1).toBe(1)
+		return 1
+	})
+
+	test('second', async () => {
+		expect(1).toBe(1)
+		return 2
+	})
+
+	test('third', async () => {
+		expect(1).toBe(1)
+		return 3
+	})
+
+	test.only('with await', async ({result}) => {
+		const third = await result('await multiple prior test result | third')
+		expect(third).toBe(3)
+
+		const second = await result('await multiple prior test result | second')
+		expect(second).toBe(2)
+
+		const first = await result('await multiple prior test result | first')
+		expect(first).toEqual(1)
+	})
+})
+
+test.only('supports passing options', {repeats: 2}, context => {
+	expect(context.task.repeats).toBe(2)
+})
+
+describe.only('extendable', () => {
+	const newTest = test.extend({
+		foo: 'bar',
+		// eslint-disable-next-line no-empty-pattern
+		baz: async ({}, use) => {
+			await use('qux')
+		},
+	})
+	newTest('check non func', ({foo}) => {
+		expect(foo).toBe('bar')
+		return 'bar'
+	})
+	newTest('check func', async ({baz, result}) => {
+		expect(baz).toBe('qux')
+		await expect(result('extendable | check non func')).resolves.toBe('bar')
+	})
+})
---
+++ test/cli/test/custom-runner.test.ts
@@ -0,0 +1,15 @@
+import { expect, test } from 'vitest'
+import { runVitest } from '../../test-utils'
+
+test('can run custom runner with Vitest', async () => {
+  const vitest = await runVitest({
+    root: './fixtures/custom-runner',
+    reporters: [['default', { isTTY: false }]],
+  })
+
+  expect(vitest.stderr).toMatchInlineSnapshot(`""`)
+
+  expect(vitest.stdout).toContain('✓ custom-runner.test.ts')
+  expect(vitest.stdout).toContain('Test Files  1 passed')
+  expect(vitest.stdout).toContain('Tests  4 passed | 3 skipped')
+})
